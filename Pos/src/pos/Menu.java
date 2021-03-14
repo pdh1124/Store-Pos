ab4017a20f5e1305cd7cacd8d52dee4ce22938b8
pdh1124 <pdw3231@naver.com>
2021-03-15 00:25:33 +0900

    출력문

---
+++ Pos/src/pos/Menu.java
@@ -0,0 +1,15 @@
+package pos;
+
+public class Menu {
+	static void showProduct() {
+		System.out.println("----------------");
+		System.out.println("자바마트에 방문하신 것을 환영합니다.");
+		System.out.println("----------------");
+		System.out.println("1. 상품 관리");
+		System.out.println("2. 상품 주문");
+		System.out.println("3. 주문 내역");
+		System.out.println("4. 현재 매출");
+		System.out.println("5. 프로그램 종료");
+		System.out.println("----------------");	
+	}
+}
